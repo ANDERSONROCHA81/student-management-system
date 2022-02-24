c1ec8841699bd91592d91e12c3d46f2162b2101f
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-02-24 19:42:25 -0300

    Adição de um método na classe StudentController

--- src/main/java/net/javaguides/sms/controllers/StudentController.java
+++ src/main/java/net/javaguides/sms/controllers/StudentController.java
@@ -1,8 +1,22 @@
 package net.javaguides.sms.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import net.javaguides.sms.services.StudentService;
 
 @Controller
 public class StudentController {
-
+	
+	@Autowired
+	private StudentService studentService;
+	
+	// Handler method to handle list students and return model and view
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
 }
