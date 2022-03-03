f5b37a757157b66a1739526a55849d578f684211
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-03-03 19:33:20 -0300

    Adição de alguns estudantes e adição de um botão na view students.html

--- src/main/java/net/javaguides/sms/entities/Student.java
+++ src/main/java/net/javaguides/sms/entities/Student.java
@@ -28,9 +28,8 @@ public class Student {
 		
 	}
 	
-	public Student(Long id, String firstName, String lastName, String email) {
+	public Student(String firstName, String lastName, String email) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
