190669ba625732fbf1ffece8c605435e7d2fff4e
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-11-29 22:19:55 -0300

    Criação dos métodos para deletar um estudante

--- src/main/java/net/javaguides/sms/services/impl/StudentServiceImpl.java
+++ src/main/java/net/javaguides/sms/services/impl/StudentServiceImpl.java
@@ -35,4 +35,9 @@ public class StudentServiceImpl implements StudentService{
 		return studentRepository.save(student);
 	}
 
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);
+	}
+
 }
